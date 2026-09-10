2f4c504c2d2af573ba486f7a3333ef3fdb3aab53
Maycon Fabio <m.vinnicyus@gmail.com>
2026-09-09 21:18:04 -0300

    fix(test): Set test timeout to 15 seconds

--- web/vite.config.ts
+++ web/vite.config.ts
@@ -21,6 +21,7 @@ export default defineConfig(({ mode }) => {
       setupFiles: ['./vitest.setup.ts'],
       passWithNoTests: true,
       restoreMocks: true, // avoids having to call `afterEach(() => vi.restoreAllMocks())`
+      testTimeout: 15_000,
     },
   };
 });
